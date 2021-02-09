703ddab508ab65d81f21b83f8abfc430b1e79bc6
Sara <saragorleta@yahoo.com>
2021-02-09 07:46:29 +0100

    stampa dieci email

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var app = new Vue({
   },
   mounted() {
 
-    for(let i = 0; i < 5; i++){
+    for(let i = 0; i < 10; i++){
       axios
       .get('https://flynn.boolean.careers/exercises/api/random/mail')
       .then((result) => {
